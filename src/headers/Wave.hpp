a5ddbec30ba77692202beccf04ea9dd0647663f4
C3mR4de <NVadim2005@gmail.com>
2024-12-29 20:24:34 +0300

    Added wave game mode basic implementation

--- src/headers/Wave.hpp
+++ src/headers/Wave.hpp
@@ -1,26 +1,99 @@
 #ifndef WAVE_HPP
 #define WAVE_HPP
 
+#include <cstddef>
+#include <cmath>
+#include <array>
+#include <atomic>
+#include <SFML/Graphics/Drawable.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/Graphics/RenderStates.hpp>
 #include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/VertexArray.hpp>
+#include <SFML/System/Angle.hpp>
 
 namespace rd
 {
-    class Wave
+    template <std::size_t trail_length = 200>
+    struct Wave
     {
-    public:
+        Wave() = delete;
 
-        Wave(const sf::Texture& texture);
-        Wave(sf::Texture&& texture) = delete;
-        Wave& operator=(const sf::Texture& texture);
-        Wave& operator=(sf::Texture&& texture) = delete; 
-        ~Wave() noexcept = default;
+        struct State
+        {
+            enum Gravity: short int
+            {
+                Reversed = -1,
+                Normal = 1
+            };
 
-        Wave& update();
+            sf::Vector2f position;
+            sf::Vector2f speed;
+            Gravity gravity;
+            sf::Angle angle;
+            float thickness;
+            std::array<sf::Vector2f, trail_length> trail;
 
-    private:
+            State& update(float dt, bool is_key_pressed);
+        };
 
-        sf::Sprite sprite;
+        struct Entity: public sf::Drawable
+        {
+            Entity(const sf::Texture& texture);
+            Entity& update(const State& state);
+            void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+
+            sf::Sprite sprite;
+            sf::VertexArray trail = sf::VertexArray(sf::PrimitiveType::TriangleStrip, trail_length);
+        };
     };
+}
+
+template <std::size_t trail_length>
+rd::Wave<trail_length>::State& rd::Wave<trail_length>::State::update(float dt, bool is_key_pressed)
+{
+    angle = sf::degrees(45.f * static_cast<float>(gravity) * (is_key_pressed ? 1.f : -1.f));
+    speed.y = speed.x * std::tan(angle.asRadians());
+    position += speed * dt;
+
+    for (std::size_t i = trail_length - 1; i > 1; --i)
+    {
+        trail[i] = trail[i - 2];
+    }
+
+    trail[0] = {position.x, position.y - thickness / 2.f};
+    trail[1] = {position.x, position.y + thickness / 2.f};
+
+    return *this;
+}
+
+template <std::size_t trail_length>
+rd::Wave<trail_length>::Entity::Entity(const sf::Texture& texture):
+    sprite(texture)
+{
+    sprite.setOrigin(sf::Vector2f(texture.getSize()) / 2.f);
+}
+
+template <std::size_t trail_length>
+rd::Wave<trail_length>::Entity& rd::Wave<trail_length>::Entity::update(const State& state)
+{
+    sprite.setPosition(state.position);
+    sprite.setRotation(state.angle);
+    
+    for (std::size_t i = 0; i < trail_length; ++i)
+    {
+        trail[i] = {state.trail[i], sf::Color::White};
+    }
+
+    return *this;
+}
+
+template <std::size_t trail_length>
+void rd::Wave<trail_length>::Entity::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+    target.draw(trail, states);
+    target.draw(sprite, states);
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,19 @@
+#include <cstddef>
 #include <string>
 #include <thread>
 #include <atomic>
+#include <memory>
 #include <functional>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Graphics/Font.hpp>
 #include <SFML/Graphics/Text.hpp>
 #include "headers/TickCounter.hpp"
+#include "headers/Wave.hpp"
 
-void render(sf::RenderWindow& window, std::atomic<rd::TickCounter>& tps_counter)
+constexpr std::size_t trail_length = 40000;
+using Wave = rd::Wave<trail_length>;
+
+void render(sf::RenderWindow& window, std::atomic<rd::TickCounter>& tps_counter, std::atomic<Wave::State>& wave_state)
 {
     const bool active = window.setActive();
 
@@ -19,13 +25,25 @@ void render(sf::RenderWindow& window, std::atomic<rd::TickCounter>& tps_counter)
     sf::String fps_string = "FPS: ";
     sf::String tps_string = "TPS: ";
 
+    sf::Texture texture("wave_head.png");
+    Wave::Entity wave_entity(texture);
+
+    sf::View view(window.getDefaultView());
+
+    window.setFramerateLimit(5);
+
     while (window.isOpen())
     {
+        wave_entity.update(wave_state.load());
+        view.setCenter({wave_entity.sprite.getPosition().x + 200.f, view.getCenter().y});
+        window.setView(view);
+        
         fps_counter.update();
-        
         debug_info.setString(fps_string + std::to_string(fps_counter.getTps()) + "\n" + tps_string + std::to_string(tps_counter.load().getTps()));
-        
+        debug_info.setPosition(view.getCenter() - view.getSize() / 2.f);
+
         window.clear();
+        window.draw(wave_entity);
         window.draw(debug_info);
         window.display();
     }
@@ -42,12 +60,22 @@ int main()
     };
 
     std::atomic<rd::TickCounter> tps_counter;
-    std::jthread rendering_thread(&render, std::ref(window), std::ref(tps_counter));
-    
+    const auto [x, y] = window.getDefaultView().getCenter();
+    std::atomic<Wave::State> wave_state(Wave::State{{0, x}, {1000, 0}, Wave::State::Gravity::Normal, sf::degrees(45.f), 30.f});
+    std::jthread rendering_thread(&render, std::ref(window), std::ref(tps_counter), std::ref(wave_state));
+
+    sf::Clock frame_clock;
+
     while (window.isOpen())
     {
+        float dt = frame_clock.restart().asSeconds();
         window.handleEvents(on_close);
+
         tps_counter.store(tps_counter.load().update());
+        wave_state.store(wave_state.load().update(dt, sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space)));
+
+        // using namespace std::chrono_literals;
+        // std::this_thread::sleep_for(2ms);
     }
 }
 
